8be5c80be5fca7c4c1833f07495ea53283385062
Dante Spencer <dantespe@umich.edu>
2018-12-20 21:58:48 -0500

    Final 0.0.1b

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from disutils.core import setup
 
-setup(name='pyextras',
-      version='0.0.1',
-      description='Extra Python utilities.',
-      author='Dante Spencer',
-      author_email='dantespe@umich.edu',
-      packages=['cache'],
-     )
+setup(
+    name='pyextras',
+    version='0.0.1',
+    description='Extra Python utilities.',
+    author='Dante Spencer',
+    author_email='dantespe@umich.edu',
+    # packages=find_packages()
+    packages=['pyextras', 'pyextras.cache',]
+)
